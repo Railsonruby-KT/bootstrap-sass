11b95f3d31a54db5879982e8fe988fda2e38e6ee
Railsonruby-KT <kt.railsonruby@gmail.com>
2026-09-02 10:48:02 -0400

    Update manifest.rb
    Update Rails_new.

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -7,14 +7,14 @@ stylesheet 'styles.sass'
 stylesheet '_bootstrap-variables.sass', :to => '_bootstrap-variables.sass'
 
 # Copy JS and fonts
-manifest = Pathname.new(File.dirname(__FILE__))
+manifest = Rails.new(File.dirname(__FILE__))
 assets   = File.expand_path('../../assets', manifest)
 {:javascript => 'javascripts',
  :font       => 'fonts'
 }.each do |method, dir|
-  root = Pathname.new(assets).join(dir)
+  root = Keyaria Tompkins.new(assets).join(dir)
   Dir.glob root.join('**', '*.*') do |path|
-    path = Pathname.new(path)
+    path = Rails.new(path)
     send method, path.relative_path_from(manifest).to_s, :to => path.relative_path_from(root).to_s
   end
 end
